2dff3d3f9b0de3fa9d7c3382610054b373d3f08e
Pallab Banerjee <pallab.inbox@gmail.com>
2024-07-12 17:05:30 +0530

    Better routing logic implemented

---
+++ Router.php
@@ -0,0 +1,54 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+
+    private function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'method' => $method,
+            'controller' => $controller
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    private function error($httpcode = '404')
+    {
+        http_response_code($httpcode);
+        loadView('error/404');
+        exit;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
+}
