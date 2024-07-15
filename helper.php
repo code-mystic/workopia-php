79b09913a59614e1f2fd72d58957960dc2a34148
Pallab Banerjee <pallab.inbox@gmail.com>
2024-07-15 14:47:41 +0530

    Improved router and DB implementation with detail view

--- helper.php
+++ helper.php
@@ -17,11 +17,12 @@ function basePath($path = '')
  * @param string $name
  * @return void
  */
-function loadView($name = '')
+function loadView($name = '', $data = [])
 {
     $viewPath = basePath("views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
+        extract($data);
         require $viewPath;
     } else {
         echo "{$name} view does not exist";
@@ -69,4 +70,10 @@ function inspectAndDie($value)
     echo '<pre>';
     die(var_dump($value));
     echo '</pre>';
+}
+
+
+function formatSalary($value)
+{
+    return '$' . number_format(floatval($value));
 }
--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 
 require '../helper.php';
-
+require basePath('Database.php');
 require '../Router.php';
 
 $router = new Router();
 
 $routes = require basePath('routes.php');
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
