c018fb9a9caa5f4aa75bbc0d7ad8ab3b3fe0a94f
Pallab Banerjee <pallab.inbox@gmail.com>
2024-07-15 17:06:52 +0530

    Integrated with composer and PS4 auloading according to namespace

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
 require '../helper.php';
-require basePath('Framework/Database.php');
-require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+// require basePath('Framework/Router.php');
+
+use Framework\Router;
 
 $router = new Router();
 
